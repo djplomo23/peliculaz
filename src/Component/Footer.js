579d75c0c3496d2b2c34ba166397c78798b891a8
djplomo23 <69323758+djplomo23@users.noreply.github.com>
2022-09-17 15:08:37 -0400

    pubglicadad

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -5,7 +5,7 @@ export const Footer = () => {
     <footer className='footer'>
     <img src={logo} alt='peliculaz'/>
    
-<iframe class="na" frameborder="0" scrolling="no" width="300" height="250" sandbox="allow-scripts allow-popups allow-forms allow-same-origin" src="//a.shukriya90.com/api/spots/401022?p=1&s1=%subid1%&kw="></iframe>
+
 
     </footer>
   )
